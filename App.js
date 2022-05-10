0946f18c6930db3c0478892591941267675a9e4d
Wai Yin Tan <waiyinta@gmail.com>
2022-05-10 15:24:08 -0400

    added icon styles

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect, Component } from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./components/LoginScreen";
-import HomeScreen from "./components/HomeScreen";
-import AddTrip from "./components/AddTrip";
-import Tabs from "./components/Tabs";
 import store from "./components/store";
 import { Provider } from "react-redux";
-import SignUpScreen from "./components/SignUpScreen";
 import TabNavigator from "./components/navigation/TabNavigator";
 import InitialNavigator from "./components/navigation/InitialNavigator";
 import { auth } from "./firebase";
-
-const Stack = createNativeStackNavigator();
 
 export default class App extends Component {
   constructor() {
@@ -39,21 +31,6 @@ export default class App extends Component {
       <Provider store={store}>
         <NavigationContainer>
           {this.state.isLoggedIn? <TabNavigator/> : <InitialNavigator/>}
-          {/* <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Login"
-            component={LoginScreen}
-          />
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="SignUp"
-            component={SignUpScreen}
-          />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Add a trip" component={AddTrip} />
-          <Stack.Screen name = "Tabs" component={Tabs}/>
-        </Stack.Navigator> */}
         </NavigationContainer>
       </Provider>
     );
--- components/navigation/TabNavigator.js
+++ components/navigation/TabNavigator.js
@@ -3,27 +3,57 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
-//screen stacks
-import { ChatStackNavigator, ExploreStackNavigator, HomeStackNavigator, ProfileStackNavigator } from "./StackNavigator";
+//Screen Stacks
+import {
+  ChatStackNavigator,
+  ExploreStackNavigator,
+  HomeStackNavigator,
+  ProfileStackNavigator,
+} from "./StackNavigator";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
     <Tab.Navigator
-    initialRouteName="HomeTab"
-    screenOptions={{
-      unmountOnBlur:true
-    }}>
-    <Tab.Screen name="HomeTab" component={HomeStackNavigator} />
-    <Tab.Screen name="ChatTab" component={ChatStackNavigator} />
-    <Tab.Screen name="ExploreTab" component={ExploreStackNavigator} />
-    <Tab.Screen name = "ProfileTab" component = {ProfileStackNavigator} />
+      initialRouteName="HomeTab"
+      screenOptions={({ route }) => ({
+        unmountOnBlur:true,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#BABFE0",
+        tabBarInactiveTintColor: "#BABFE0",
+        tabBarStyle: {
+          padding: 10,
+          height: 70,
+        },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let rn = route.name;
+
+          if (rn === "HomeTab") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (rn === "ChatTab") {
+            iconName = focused
+              ? "chatbubble-ellipses-sharp"
+              : "chatbubble-ellipses-outline";
+          } else if (rn === "ExploreTab") {
+            iconName = focused ? "search-circle" : "search-circle-outline";
+          } else if (rn === "ProfileTab") {
+            iconName = focused ? "person-circle" : "person-circle-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="HomeTab" component={HomeStackNavigator} />
+      <Tab.Screen name="ChatTab" component={ChatStackNavigator} />
+      <Tab.Screen name="ExploreTab" component={ExploreStackNavigator} />
+      <Tab.Screen name="ProfileTab" component={ProfileStackNavigator} />
     </Tab.Navigator>
+  );
+};
 
-  )
-}
+export default TabNavigator;
 
-export default TabNavigator
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
