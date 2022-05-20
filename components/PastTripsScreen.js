0ab0a8018368a582b34f60ae75959d0d97ec2bf8
dleksql129 <dleethacker@gmail.com>
2022-05-20 16:55:50 -0400

    current and past trips work

--- components/PastTripsScreen.js
+++ components/PastTripsScreen.js
@@ -12,26 +12,46 @@ const PastTripsScreen = () => {
   const tripInfo = useSelector((state) => state.trips.trips);
   const userCurrentTrips = userInfo.trip;
 
+  console.log("pastTrips - tripInfo", tripInfo);
+  // console.log(
+  //   "past time figureout ",
+  //   Math.floor(Date.now() / 1000) - tripInfo[0].endDate.seconds
+  // );
+  console.log("date now in seconds", Math.floor(Date.now() / 1000));
+  console.log("lastdate in trip", tripInfo[0].endDate.seconds);
+
+  // seconds = new Date().getTime() / 1000;
+  // {console.log(
+  //   "after filter -tripInfo",
+  //   tripInfo.filter((trip) => {
+  //     Math.floor(Date.now()) - trip.endDate.seconds > 0;
+  //   })
+  // )}
+
   return (
     <View style={styles.container}>
       <Center>
         {tripInfo &&
-          tripInfo.map((trip, index) => (
-            <Button
-              key={index}
-              variant="outline"
-              style={styles.tripList}
-              _text={styles.tripButton}
-              onPress={() =>
-                navigation.navigate("Trip", {
-                  trip,
-                  tripId: userCurrentTrips[index],
-                })
-              }
-            >
-              {trip.tripName}
-            </Button>
-          ))}
+          tripInfo
+            .filter(
+              (trip) => Math.floor(Date.now() / 1000) - trip.endDate.seconds > 0
+            )
+            .map((trip, index) => (
+              <Button
+                key={index}
+                variant="outline"
+                style={styles.tripList}
+                _text={styles.tripButton}
+                onPress={() =>
+                  navigation.navigate("Trip", {
+                    trip,
+                    tripId: userCurrentTrips[index],
+                  })
+                }
+              >
+                {trip.tripName}
+              </Button>
+            ))}
       </Center>
     </View>
   );
